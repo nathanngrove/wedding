e17f785e8b36c7da878ce4fb0fe0502fe6859167
Nathan Grove <nathangrove08@gmail.com>
2025-07-06 23:08:02 -0700

    test

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
 	basePath: "/nathanbridgetwedding",
-	output: "export",
+	output: "standalone",
 	images: { unoptimized: true },
 };
 
